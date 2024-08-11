e18cca085878db12d3aeecdcf1e4f27eb3bf14b1
ustinian-wang <wangjser@gmail.com>
2024-08-11 14:42:16 +0800

    optimize: simply state

--- src/App.jsx
+++ src/App.jsx
@@ -8,52 +8,40 @@ import { RecordManager } from "./business/RecordManager";
 import { getWinner } from "./business/game";
 
 export default function MyApp() {
-    let grids = getGrids();
-    let role = RoleManager.getCurrRole();
-    let roles = RoleManager.getRoles();
-    let records = RecordManager.getRecords();
-    let currRecords = RecordManager.getCurrentRecords();
-    // let records = RecordHistory.getRecords();
-    const [gridsValue, setGridsValue] = useState(grids);
-    const [roleValue, setRoleValue] = useState(role);
-    const [rolesValue, setRolesValue] = useState(roles);
-    const [recordsValue, setRecordsValue] = useState(records);
-    const [currRecordsValue, setCurrRecordsValue] = useState(currRecords)
-
-
 
     const onClickGrid = (index)=>{
-        let grid = grids[index];
-        let winner = getWinner(roles, grids);
+        let grid = state.grids[index];
+        let grids = getGrids();
+        let role = RoleManager.getCurrRole();
+        let roles = RoleManager.getRoles();
+        let records = RecordManager.getRecords();
+        let winner = getWinner(state.roles, state.grids);
+        console.log("jser winner", winner);
         if(grid.use(role.roleId) && !winner){
-
-           //交换角色
-           role = RoleManager.exchangeRoles();
-           setRoleValue(role);
-           roles = RoleManager.getRoles();
-           setRolesValue(roles);
-
-           //增加一条操作记录
-           RecordManager.add(role.roleId, grid.gridId);
-           records = RecordManager.getRecords();
-           currRecords = RecordManager.getCurrentRecords();
-           setRecordsValue(records);
-           setCurrRecordsValue(currRecords);
+            grids = getGrids();
+            roles = RoleManager.getRoles();
+            role = RoleManager.exchangeRoles();
+            RecordManager.add(role.roleId, grid.gridId);
+            records = RecordManager.getRecords();
+            updateState({
+                role,
+                grids,
+                roles,
+                records
+            })
         }
-        setGridsValue(grids);
+
     }
 
     const onClickRecord = (index)=>{
         RecordManager.changeCursor(index);
-
-        currRecords = RecordManager.getCurrentRecords();
-        setCurrRecordsValue(currRecords);
+        let currRecords = RecordManager.getCurrentRecords();
         initGridsByCurrRecords(currRecords);
     }
 
     function initGridsByCurrRecords(currRecords){
         const gridsMapById = {};
-        grids.forEach(grid=>{
+        state.grids.forEach(grid=>{
             gridsMapById[grid.gridId] = grid;
             grid.clear();
         })
@@ -67,18 +55,35 @@ export default function MyApp() {
             grid.use(roleId);
         })
 
-        setGridsValue(grids);
+        updateState({
+            grids: state.grids
+        })
     }
 
+    const [state, setState] = useState(()=>{
+        return {
+            grids : getGrids(),
+            role : RoleManager.getCurrRole(),
+            roles : RoleManager.getRoles(),
+            records : RecordManager.getRecords(),
+        }
+    })
+
+    const updateState = (value)=>{
+        setState({
+            ...state,
+            ...value
+        })
+    }
 
     return (
         <div style={{display: 'flex'}}>
             <div>
-                <GameProcess role={roleValue} roles={rolesValue} grids={gridsValue}></GameProcess>
-                <GridTable grids={gridsValue} role={roleValue} onClickGrid={onClickGrid}/>
+                <GameProcess role={state.role} roles={state.roles} grids={state.grids}/>
+                <GridTable grids={state.grids} role={state.role} onClickGrid={onClickGrid}/>
             </div>
             <div>
-                <RecordHistory records={recordsValue} onClickRecord={onClickRecord}></RecordHistory>
+                <RecordHistory records={state.records} onClickRecord={onClickRecord}/>
             </div>
         </div>
 
